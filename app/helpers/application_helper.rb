8c59074cc3688d0a47956510a06c301864e6bc1a
stephen <85636682@qq.com>
2014-03-01 10:42:53 +0800

    devise_message commentable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
-module ApplicationHelper  
+module ApplicationHelper
+  def notice_message
+    flash_messages = []
+
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close", 'data-dismiss' => "alert") + message, :class => "alert alert-danger")
+      flash_messages << text if message
+    end
+
+    flash_messages.join("\n").html_safe
+  end
+  
 	def active_controller?(c_name = nil)
     raw('class="active"') if controller_name == c_name
   end
