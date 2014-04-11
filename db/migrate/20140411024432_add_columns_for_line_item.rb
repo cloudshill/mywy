ec9e5ff0b581721fa1d2c245822f5585dd32ae6e
stephen <85636682@qq.com>
2014-04-11 18:23:38 +0800

    line item polymorphic

--- db/migrate/20140411024432_add_columns_for_line_item.rb
+++ db/migrate/20140411024432_add_columns_for_line_item.rb
@@ -3,7 +3,5 @@ class AddColumnsForLineItem < ActiveRecord::Migration
     add_column :line_items, :line_itemable_type, :string
     add_column :line_items, :line_itemable_id, :integer
     remove_column :line_items, :product_id, :integer
-    remove_column :line_items, :itemable_type, :string
-    remove_column :line_items, :itemable_id, :integer
   end
 end
