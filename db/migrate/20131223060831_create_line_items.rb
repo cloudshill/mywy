c1f548fefd3eb6360e9b36bbe47c504a319bad61
stephen <85636682@qq.com>
2014-04-11 18:31:04 +0800

    line item polymorphic

--- db/migrate/20131223060831_create_line_items.rb
+++ db/migrate/20131223060831_create_line_items.rb
@@ -3,8 +3,10 @@ class CreateLineItems < ActiveRecord::Migration
     create_table :line_items do |t|
       t.integer :amount, :default => 1
       t.integer :product_id
-	  t.integer :member_id
-	  t.integer :order_id
+      t.integer :member_id
+      t.integer :order_id
+      t.string  :line_itemable_type
+      t.integer :line_itemable_id
 
       t.timestamps
     end
--- db/migrate/20140411024432_add_columns_for_line_item.rb
+++ db/migrate/20140411024432_add_columns_for_line_item.rb
@@ -1,7 +1,5 @@
 class AddColumnsForLineItem < ActiveRecord::Migration
   def change
-    add_column :line_items, :line_itemable_type, :string
-    add_column :line_items, :line_itemable_id, :integer
     remove_column :line_items, :product_id, :integer
   end
 end
