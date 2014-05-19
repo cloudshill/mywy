a587191fc0442951e9346f430d7c00272a4af386
stephen <85636682@qq.com>
2014-05-19 14:10:56 +0800

    action view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < Devise::SessionsController
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    respond_with(resource, serialize_options(resource)) do |format|
+      format.html
+      format.html.phone
+      format.html.tablet
+    end
+  end
 end
