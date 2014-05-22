f1d542af63d4bffe6144935aa8bef76957167714
stephen <85636682@qq.com>
2014-05-22 09:53:07 +0800

    显示数据数量

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @movies = Movie.all.limit(10)
-    @products = Product.all.order("sales ASC").limit(10)
-    @foods = Food.all.order("name ASC").limit(10)
   end
 end
