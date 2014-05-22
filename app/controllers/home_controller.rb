a09e6fc88a524941bd0d208905295bfee38e00a8
stephen <85636682@qq.com>
2014-05-22 08:31:40 +0800

    css调整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.all.limit(10)
     @products = Product.all.order("sales ASC").limit(10)
+    @foods = Food.all.order("name ASC").limit(10)
   end
 end
