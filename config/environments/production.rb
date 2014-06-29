f7585e79039a580bde609df115bf1fed1a3e5a4a
stephen <85636682@qq.com>
2014-06-29 10:43:39 +0800

    添加mobile.js编译

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Mywy::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( application.mobile.css cpanel.css ui-dialog.css cpanel.js wap.css wap.js essage.js dialog-min.js )
+  config.assets.precompile += %w( application.mobile.css cpanel.css ui-dialog.css application.mobile.js cpanel.js wap.css wap.js essage.js dialog-min.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
